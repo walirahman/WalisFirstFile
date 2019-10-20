9a744d13c26bcf278602c5de18177f94f84eb1c3
walirahman <46411630+walirahman@users.noreply.github.com>
2019-10-20 13:05:36 -0500

    New file for your

---
+++ CmdPractice1.java
@@ -0,0 +1,8 @@
+public class CmdPractice1{
+
+public static void main(String[] args){
+
+System.out.println("Cmd command promt");
+
+ }
+}
---
+++ GoodBye.java
@@ -0,0 +1,10 @@
+public class GoodBye {
+      public static void main (String[] args){
+
+             System.out.println("Good Bye");
+
+      }
+
+
+
+}
